6758f00abbbe1779cf9f7d5c3d625b73b0ed9f1a
farhan009 <48472171+farhan009@users.noreply.github.com>
2019-10-20 02:57:58 -0700

    ActionBar with Option Menu, ListView,SearchBar, Gradient, Color

---
+++ app/src/main/java/com/example/actionbar/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.actionbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    String[] country;
+    ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setTitle("Country Names");
+
+        listView = findViewById(R.id.listView_id);
+        country = getResources().getStringArray(R.array.country);
+
+        arrayAdapter = new ArrayAdapter(MainActivity.this, R.layout.sample_view, R.id.textView_id, country);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_item, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.search_id);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.setting_id){
+            Toast.makeText(MainActivity.this, "Setting", Toast.LENGTH_SHORT).show();
+        }
+
+        if (item.getItemId() == R.id.share_id){
+            Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
+        }
+
+        if (item.getItemId() == R.id.feedback_id){
+            Toast.makeText(MainActivity.this, "Feedback", Toast.LENGTH_SHORT).show();
+        }
+
+        if (item.getItemId() == R.id.aboutus_id){
+            Toast.makeText(MainActivity.this, "About Us", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
