f4ba627e400c0ad3cbe4287575202c87e8fc519e
farhan009 <48472171+farhan009@users.noreply.github.com>
2019-10-29 22:47:49 +0600

    ActionBar with Option Menu, ListView,SearchBar, Gradient, Color, Default Back Button

--- app/src/main/java/com/example/actionbar/WelcomePage.java
+++ app/src/main/java/com/example/actionbar/WelcomePage.java
@@ -13,16 +13,17 @@ public class WelcomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
 
+        // To adding back button on Action Bar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    /*@Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {   //manifest file er maddhoma o back a asa jai
 
         if (item.getItemId() == android.R.id.home){
             finish();
         }
         return super.onOptionsItemSelected(item);
-    }
+    } */
 }
